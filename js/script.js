6da651063af88044aaf467638b402fa6a67096c5
roberto villablanca <roberto.villablanca.1996@gmail.com>
2023-06-16 14:01:43 -0400

    primer js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 let precio = 420;
+
+let totalCantidad = document.querySelector("#cantidad");
+let totalColor = document.querySelector("#color");
+
+let button = document.querySelector("#consultar");
+
+button.addEventListener("consultar", () => {
+  document.querySelector("#total").innerHTML = totalCantidad.value * precio;
+  document.querySelector("#totalCantidad").innerHTML = totalCantidad.value;
+  document.querySelector("#color").style.backgroundColor = color.value;
+});
